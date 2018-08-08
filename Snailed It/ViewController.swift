9497cb1a73f2e8f8a6fb3659a44a954a12700ac4
Kode Klossy <kodewithklossy@kodes-macbook-air-5.local>
2018-08-08 14:25:03 -0500

    Initial Commit

---
+++ Snailed It/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snailed It
+//
+//  Created by Kode With Klossy on 8/8/18.
+//  Copyright © 2018 Kode With Klossy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
